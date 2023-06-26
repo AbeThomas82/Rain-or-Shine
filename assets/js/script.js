f59c2cf3b0f9693694726668c434d357cb3b4ead
Sherwin Abraham <sherwinabraham@gmail.com>
2023-06-26 14:55:52 -0500

    Added fetch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
+var enterCity = document.querySelector('#enterCity');
+var addCity = document.querySelector('#addCity');
+var displayCity = document.querySelector('#displayCity');
+var date = document.querySelector("#date");
+var gist = document.querySelector('#gist');
+var temperature = document.querySelector('#temperature');
+var windspeed = document.querySelector('#wind-speed');
+var humidity = document.querySelector('#humidity');
 var weatherUrl = 'https://api.openweathermap.org';
 var weatherKey = 'e58ac91e0a6a8f397405a4f6e3d97d37';
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 function fetchCoords(){
-    var apiUrl = `${weatherUrl}/geo/1.0/direct?q=houston&limit=5&appid=${weatherKey}`;
+    var apiUrl = [weatherUrl + "/geo/1.0/direct?q="+ enterCity +"&limit=5&appid="+weatherKey];
 fetch(apiUrl)
     .then(function (res) {
         console.log(res)
