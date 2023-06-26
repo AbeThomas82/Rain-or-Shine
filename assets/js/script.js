eb530838fc87679ab102c33ae90c13e9ba92fa4e
Sherwin Abraham <sherwinabraham@gmail.com>
2023-06-26 16:48:39 -0500

    Modified more js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var weatherKey = 'e58ac91e0a6a8f397405a4f6e3d97d37';
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 function fetchCoords(){
-    var apiUrl = [weatherUrl + "/geo/1.0/direct?q="+ enterCity +"&limit=5&appid="+weatherKey];
+    var apiUrl = [weatherUrl + "/geo/1.0/direct?q="+ enterCity.value +"&limit=5&appid="+weatherKey];
 fetch(apiUrl)
     .then(function (res) {
         console.log(res)
@@ -27,5 +27,9 @@ fetch(apiUrl)
     });
 }
 
-
-fetchCoords();
+const searchForm = document.querySelector('#search-form');
+searchForm.addEventListener('submit', function (event){
+    event.preventDefault();
+    fetchCoords();
+    enterCity.value = '';
+});
